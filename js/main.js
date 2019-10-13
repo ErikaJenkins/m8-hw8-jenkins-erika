f9d9b110d82c65e620ef8370b26e8f8c20171aa7
Erika Jenkins <erikamjenkins@ufl.edu>
2019-10-13 19:15:26 -0400

    final code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-// It is always helpful to use comments in your code!
+
+var myMainNavLinks = document.getElementsByClassName('main-menu-labels');
+
+function toggleSubMenu(){
+  console.log(this.nextElementSibling);
+  this.nextElementSibling.classList.toggle('hide');
+}
+
+for(i=0; i<myMainNavLinks.length; i++){
+  //myMainNavLinks[i].addEventListener('click', toggleSubMenu);
+  myMainNavLinks[i].addEventListener('mouseover', toggleSubMenu);
+  //myMainNavLinks[i].addEventListener('mouseenter', toggleSubMenu);
+  myMainNavLinks[i].addEventListener('mouseleave', toggleSubMenu);
+}
+
+/*
+var myLinks = document.getElementsByClassName('sub-menu hide');
+var menuBackAfterMouse = document.getElementsByClassName('show');
+
+function toggleSubMenu(){
+  console.log(this.nextElementSibling);
+  this.nextElementSibling.classList.toggle('hide');
+}
+function minusSubMenu(){
+  console.log(this.nextElementSibling);
+  this.nextElementSibling.classList.add('show');
+}
+
+for(i=0; i<myMainNavLinks.length; i++){
+  //myMainNavLinks[i].addEventListener('click', toggleSubMenu);
+  myMainNavLinks[i].addEventListener('mouseover', toggleSubMenu);
+  //myMainNavLinks[i].addEventListener('mouseenter', toggleSubMenu);
+  myMainNavLinks[i].addEventListener('mouseleave', toggleSubMenu);
+}*/
+
+//for(i=0; i<myMainNavLinks.length; i++){
+  //myMainNavLinks[i].addEventListener('click', toggleSubMenu);
+  //myMainNavLinks[i].addEventListener('mouseover', toggleSubMenu);
+  //myMainNavLinks[i].addEventListener('mouseenter', toggleSubMenu);
+//  myMainNavLinks[i].addEventListener('mouseout', toggleSubMenu);
+//}
+
+//if(myLinks){
+//else {
+		  //Do something else - do this when the condition is FALSE
+      //myLinks = false;
+      //menuBackAfterMouse;
+      //myMainNavLinks.nextElementSibling.classList.toggle('show');
+		//}
